39abee9a1c6ffc0a6f9a39a6364a19cfe01e4408
Gabriel Enrique <solamayo.gabriel@gmail.com>
2023-12-04 16:25:16 -0300

    Upando o Projeto

---
+++ RocketDonation/src/java/model/Endereco.java
@@ -0,0 +1,141 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author solam
+ */
+@Entity
+@Table(name = "endereco")
+@NamedQueries({
+    @NamedQuery(name = "Endereco.findAll", query = "SELECT e FROM Endereco e"),
+    @NamedQuery(name = "Endereco.findByIdendereco", query = "SELECT e FROM Endereco e WHERE e.idendereco = :idendereco"),
+    @NamedQuery(name = "Endereco.findByRua", query = "SELECT e FROM Endereco e WHERE e.rua = :rua"),
+    @NamedQuery(name = "Endereco.findByNumero", query = "SELECT e FROM Endereco e WHERE e.numero = :numero"),
+    @NamedQuery(name = "Endereco.findByCep", query = "SELECT e FROM Endereco e WHERE e.cep = :cep"),
+    @NamedQuery(name = "Endereco.findByComplemento", query = "SELECT e FROM Endereco e WHERE e.complemento = :complemento")})
+public class Endereco implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "idendereco")
+    private Integer idendereco;
+    @Basic(optional = false)
+    @Column(name = "rua")
+    private String rua;
+    @Basic(optional = false)
+    @Column(name = "numero")
+    private int numero;
+    @Basic(optional = false)
+    @Column(name = "cep")
+    private String cep;
+    @Column(name = "complemento")
+    private String complemento;
+    @JoinColumn(name = "fk_email", referencedColumnName = "email")
+    @ManyToOne(optional = false, fetch = FetchType.EAGER)
+    private Doador fkEmail;
+
+    public Endereco() {
+    }
+
+    public Endereco(Integer idendereco) {
+        this.idendereco = idendereco;
+    }
+
+    public Endereco(Integer idendereco, String rua, int numero, String cep) {
+        this.idendereco = idendereco;
+        this.rua = rua;
+        this.numero = numero;
+        this.cep = cep;
+    }
+
+    public Integer getIdendereco() {
+        return idendereco;
+    }
+
+    public void setIdendereco(Integer idendereco) {
+        this.idendereco = idendereco;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public Doador getFkEmail() {
+        return fkEmail;
+    }
+
+    public void setFkEmail(Doador fkEmail) {
+        this.fkEmail = fkEmail;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idendereco != null ? idendereco.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Endereco)) {
+            return false;
+        }
+        Endereco other = (Endereco) object;
+        if ((this.idendereco == null && other.idendereco != null) || (this.idendereco != null && !this.idendereco.equals(other.idendereco))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "model.Endereco[ idendereco=" + idendereco + " ]";
+    }
+    
+}
---
+++ RocketDonation/src/java/model/Noticia.java
@@ -0,0 +1,145 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author solam
+ */
+@Entity
+@Table(name = "noticia")
+@NamedQueries({
+    @NamedQuery(name = "Noticia.findAll", query = "SELECT n FROM Noticia n"),
+    @NamedQuery(name = "Noticia.findByIdNoticia", query = "SELECT n FROM Noticia n WHERE n.idNoticia = :idNoticia"),
+    @NamedQuery(name = "Noticia.findByTitulo", query = "SELECT n FROM Noticia n WHERE n.titulo = :titulo"),
+    @NamedQuery(name = "Noticia.findByDescricao", query = "SELECT n FROM Noticia n WHERE n.descricao = :descricao"),
+    @NamedQuery(name = "Noticia.findByData", query = "SELECT n FROM Noticia n WHERE n.data = :data"),
+    @NamedQuery(name = "Noticia.findByImgUrl", query = "SELECT n FROM Noticia n WHERE n.imgUrl = :imgUrl")})
+public class Noticia implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "id_noticia")
+    private Integer idNoticia;
+    @Basic(optional = false)
+    @Column(name = "titulo")
+    private String titulo;
+    @Basic(optional = false)
+    @Column(name = "descricao")
+    private String descricao;
+    @Basic(optional = false)
+    @Column(name = "data")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date data;
+    @Column(name = "img_url")
+    private String imgUrl;
+    @JoinColumn(name = "fk_email", referencedColumnName = "usuario_email")
+    @ManyToOne(optional = false, fetch = FetchType.EAGER)
+    private Organizacao fkEmail;
+
+    public Noticia() {
+    }
+
+    public Noticia(Integer idNoticia) {
+        this.idNoticia = idNoticia;
+    }
+
+    public Noticia(Integer idNoticia, String titulo, String descricao, Date data) {
+        this.idNoticia = idNoticia;
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.data = data;
+    }
+
+    public Integer getIdNoticia() {
+        return idNoticia;
+    }
+
+    public void setIdNoticia(Integer idNoticia) {
+        this.idNoticia = idNoticia;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public Organizacao getFkEmail() {
+        return fkEmail;
+    }
+
+    public void setFkEmail(Organizacao fkEmail) {
+        this.fkEmail = fkEmail;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idNoticia != null ? idNoticia.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Noticia)) {
+            return false;
+        }
+        Noticia other = (Noticia) object;
+        if ((this.idNoticia == null && other.idNoticia != null) || (this.idNoticia != null && !this.idNoticia.equals(other.idNoticia))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "model.Noticia[ idNoticia=" + idNoticia + " ]";
+    }
+    
+}
